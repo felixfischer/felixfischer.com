d5bbfab53fe39c8398023d2997d28b37b47cd282
Felix Fischer <felix@subtract.de>
2023-10-03 16:50:09 +0200

    Snapshot

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
 		extend: {
 			fontFamily: {
 				systemui: ['system-ui', 'sans-serif'],
-			}
+			},
+			boxShadow: {
+				'3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.7)',
+				'4xl': '1rem 1rem 2rem 0rem rgba(0, 0, 0, 0.4)',
+			  }
 		},
 	},
 	plugins: [],
